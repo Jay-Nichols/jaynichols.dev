87ec15a3b6b4e6a3749f93242ede6b8782871e3f
jay nichols <jay_nichols_3@hotmail.com>
2017-02-19 18:51:12 -0600

    fixed labelling issues

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -28,12 +28,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-         
+          <a class="navbar-brand" href="http://www.jaynichols.info">Blog</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
             <li>
-              <a href="{{ url('/') }}">Home</a>
+              <a href="{{ url('/blog') }}">Home</a>
             </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
